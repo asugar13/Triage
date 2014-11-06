781f55223aefb497a407a3ebc396e28c900b27ce
c3isterd <c3isterd@23deb5b8-21a8-4767-89d0-be71fff34244>
2014-11-06 22:37:52 +0000

    Vitals object bloodPressure and heartRate changed from int to string

--- app/Triage/src/defaultPackage/Vitals.java
+++ app/Triage/src/defaultPackage/Vitals.java
@@ -5,9 +5,9 @@ public class Vitals {
 	//The date and the time that the vital signs and symptoms were recorded.
 	private String dateandTime;
 	//Patient's blood pressure at a current time.
-	private int bloodPressure;
+	private String bloodPressure;
 	//Patient's heart rate at a current time.
-	private int heartRate;
+	private String heartRate;
 	//Patient's symptoms description at a current time.
 	private String symptomsDescription;
 	
@@ -20,8 +20,8 @@ public class Vitals {
 	
 	public Vitals(int day, int month, int year, int hour, int minutes, int bloodPressure, int heartRate, String symptomsDescription) {
 		this.dateandTime = String.valueOf(day) + "/" + String.valueOf(month) + "/" + String.valueOf(year) + "//" + String.valueOf(hour) + ":" + String.valueOf(minutes);
-		this.bloodPressure = bloodPressure;
-		this.heartRate = heartRate;
+		this.bloodPressure = String.valueOf(bloodPressure);
+		this.heartRate = String.valueOf(heartRate);
 		this.symptomsDescription = symptomsDescription;
 	}
 	
@@ -35,14 +35,14 @@ public class Vitals {
 	/**
 	 * Returns Patient's blood pressure at a current time.
 	 */
-	public int getbloodPressure() {
+	public String getbloodPressure() {
 		return bloodPressure;
 	}
 	
 	/**
 	 * Returns Patient's heart rate at a current time.
 	 */
-	public int getheartRate() {
+	public String getheartRate() {
 		return heartRate;
 	}
 	
