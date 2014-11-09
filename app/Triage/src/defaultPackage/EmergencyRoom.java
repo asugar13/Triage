eaae97fbd7e6c263825a30ad113f7125d77972cd
c3coleda <c3coleda@23deb5b8-21a8-4767-89d0-be71fff34244>
2014-11-09 00:17:44 +0000

    Changed to static

--- app/Triage/src/defaultPackage/EmergencyRoom.java
+++ app/Triage/src/defaultPackage/EmergencyRoom.java
@@ -12,76 +12,84 @@ import java.util.Map.Entry;
 
 import android.content.Context;
 import android.util.Log;
+
 /**
  * 
  * @author Asier
- *
+ * 
  */
 public class EmergencyRoom {
-	private Map<String, Patient> patients;
+	private static Map<String, Patient> patients;
+
 	/**
 	 * 
 	 * @param context
 	 * @param fileName
 	 */
-	public EmergencyRoom(Context context, String fileName){
-		this.patients = new TreeMap<String, Patient>();
-		try{
-			InputStream patients_stream = context.getAssets().open("patient_records.txt");
+
+	public static void loadPatients(Context context, String fileName) {
+		patients = new TreeMap<String, Patient>();
+		try {
+			InputStream patients_stream = context.getAssets().open(
+					"patient_records.txt");
 			populate(patients_stream);
-		}catch(Exception e){ //Change to specific exception
+		} catch (Exception e) { // Change to specific exception
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * 
 	 * @return
 	 */
-	public List<Patient> getPatients(){
+	public static List<Patient> getPatients() {
 		ArrayList<Patient> allPatients = new ArrayList<Patient>();
-		for(String key : patients.keySet()){
+		for (String key : patients.keySet()) {
 			allPatients.add(patients.get(key));
 		}
-		
+
 		return allPatients;
 	}
-	
-	public Patient getPatientByHCNum(String hCardNum){
+
+	public static Patient getPatientByHCNum(String hCardNum) {
 		return patients.get(hCardNum);
 	}
-	
+
 	/**
 	 * 
 	 * @param patients_stream
 	 * @throws FileNotFoundException
 	 */
-	public void populate(InputStream patients_stream) throws FileNotFoundException {
-	Scanner scanner = new Scanner((patients_stream));
-	String[] patient_on_file;
-	while (scanner.hasNextLine()) {
-		patient_on_file = scanner.nextLine().split(",");
-		String hcn = patient_on_file[0];
-		String birthdate = patient_on_file[2];
-		String[] name = patient_on_file[1].split(" ");
-		if (patient_on_file.length == 2){
-			patients.put(hcn, new Patient(name, birthdate, hcn, new Vitals()));
-		}
-		else{
-			String[] vitalInfo = patient_on_file[3].split("&");
-			patients.put(hcn, new Patient(name, birthdate, hcn, new Vitals(vitalInfo)));
-		}
-	}
-	scanner.close();
-	}
-	public void savePatientData(FileOutputStream outputStreamPatients){
-		try{
-			for (String hc: patients.keySet()){
-				outputStreamPatients.write((patients.get(hc).toString() + "\n").getBytes());
+	public static void populate(InputStream patients_stream)
+			throws FileNotFoundException {
+		Scanner scanner = new Scanner((patients_stream));
+		String[] patient_on_file;
+		while (scanner.hasNextLine()) {
+			
+			patient_on_file = scanner.nextLine().split(",");
+			String hcn = patient_on_file[0];
+			String birthdate = patient_on_file[2];
+			String[] name = patient_on_file[1].split(" ");
+			if (patient_on_file.length == 3) {
+				patients.put(hcn, new Patient(name, birthdate, hcn,
+						new Vitals()));
+			} else {
+				String[] vitalInfo = patient_on_file[3].split("&");
+				patients.put(hcn, new Patient(name, birthdate, hcn, new Vitals(
+						vitalInfo)));
 			}
 		}
-		catch(IOException e){
+		scanner.close();
+	}
+
+	public static void savePatientData(FileOutputStream outputStreamPatients) {
+		try {
+			for (String hc : patients.keySet()) {
+				outputStreamPatients.write((patients.get(hc).toString() + "\n")
+						.getBytes());
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
-
