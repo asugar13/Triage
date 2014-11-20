7dcd4d79585d16107846affdf81780b5e954d11c
c3ugarte <c3ugarte@23deb5b8-21a8-4767-89d0-be71fff34244>
2014-11-20 20:45:43 +0000

    Fixed bug on LoginActivity and further implementation of AddNewPatient (with crashes)

--- app/Triage/src/com/example/triage/PatientsDisplayActivity.java
+++ app/Triage/src/com/example/triage/PatientsDisplayActivity.java
@@ -40,6 +40,8 @@ public class PatientsDisplayActivity extends Activity implements OnItemSelectedL
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_patients_display);
+		Intent intent = getIntent();
+		intent.getSerializableExtra("Patient_Tag");
 		String[] spinnerOptions = getResources().getStringArray(R.array.list_options);
 		allPatientsSelection = spinnerOptions[0];
 		sortedPatientsSelection = spinnerOptions[1];
