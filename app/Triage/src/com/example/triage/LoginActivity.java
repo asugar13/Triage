1cf54622725752dd7c66bad500997130ab225500
c3coleda <c3coleda@23deb5b8-21a8-4767-89d0-be71fff34244>
2014-11-20 22:02:00 +0000

    Implemented Logging in

--- app/Triage/src/com/example/triage/LoginActivity.java
+++ app/Triage/src/com/example/triage/LoginActivity.java
@@ -2,12 +2,16 @@ package com.example.triage;
 
 import defaultPackage.EmergencyRoom;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 /**
  * Activity to manage logging in into the application
@@ -19,16 +23,28 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		EmergencyRoom.getInstance().setContext(this);
 
 	}
 
+
 	/**
-	 * The Nurse logs in to the system from the login activity and goes to the
+	 * The user logs in to the system from the login activity and goes to the
 	 * patients display activity
 	 */
-	public void logInNurse(View view) {
-		Intent intent = new Intent(this, PatientsDisplayActivity.class);
-		startActivity(intent);
+	public void logInUser(View view) {
+		String username = ((EditText) this.findViewById(R.id.userIdfield)).getText().toString();
+		String password = ((EditText) this.findViewById(R.id.passwordfield)).getText().toString();
+		boolean loginSuccessful = EmergencyRoom.getInstance().logIn(username, password);
+		
+		if(loginSuccessful){
+			Intent intent = new Intent(this, PatientsDisplayActivity.class);
+			startActivity(intent);
+		}else{
+			Toast.makeText(this, "Incorrect username and password combination", Toast.LENGTH_SHORT).show();
+		}
+		
+
 	}
 
 	@Override
