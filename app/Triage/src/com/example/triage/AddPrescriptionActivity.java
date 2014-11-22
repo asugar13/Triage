5bd3a84244c388cecb891dfb0c2b6d518f3d497b
c3coleda <c3coleda@23deb5b8-21a8-4767-89d0-be71fff34244>
2014-11-22 23:08:54 +0000

    Added onclick method for saving prescriptioninfo

--- app/Triage/src/com/example/triage/AddPrescriptionActivity.java
+++ app/Triage/src/com/example/triage/AddPrescriptionActivity.java
@@ -1,16 +1,24 @@
 package com.example.triage;
 
+import defaultPackage.Patient;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddPrescriptionActivity extends Activity {
-
+	private Patient patient;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_prescription);
+		Intent intent  = getIntent();
+		patient = (Patient) intent.getSerializableExtra("Patient_Tag");
+		
 	}
 
 	@Override
@@ -30,6 +38,16 @@ public class AddPrescriptionActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	/**
+	 * Saves the prescription information to the patient
+	 */
+	public void savePrescriptionOnClick(View view){
+		String prescriptionString = ((EditText) findViewById(R.id.prescription_text)).getText().toString();
+		patient.addPrescription(prescriptionString);
+		finish();
+		
 	}
 	
 	
